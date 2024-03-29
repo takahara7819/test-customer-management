f761fb44f5afb77e64cc7301dba610f5e2cc4988
takahara <takaharasuzuka@gmail.com>
2024-03-29 18:44:26 +0900

    詳細情報ページ レイアウト変更

--- app.js
+++ app.js
@@ -30,10 +30,10 @@ app.get("/", (req, res) => {
     });
   });
 
-  //ページ遷移に変更予定
-  // app.get("/create", (req, res) => {
-  //   res.sendFile(path.join(__dirname, "html/form.html"));
-  // });
+  //ページ遷移
+  app.get("/create", (req, res) => {
+    res.sendFile(path.join(__dirname, "html/form.html"));
+  });
 
   con.query(sql, function (err, result, fields) {
     if (err) throw err;
@@ -43,49 +43,6 @@ app.get("/", (req, res) => {
     });
   });
 });
-
-//ページ遷移
-const route = (event) => {
-  // クリック時のイベントを取得
-  event = event || window.event; //なにこれ
-  //論理演算子？右と左どっちかを返す？
-  //「window.event」で現在サイトのコードが処理しているeventを返す。意図しないものになる場合もあるから注意。
-
-  // ブラウザのデフォルト動作をキャンセル
-  event.preventDefault();
-  // ブラウザの履歴に追加
-  window.history.pushState({}, "", event.target.href);
-  handleLocation();
-};
-
-const routes = {
-  404: "html/404.html",
-  "/create": "form.html",
-};
-
-const handleLocation = async () => {
-  // 現在のパスを取得
-
-  if (typeof window !== "undefined") {
-    const path = window.location.pathname;
-  }
-
-  // 現在のパスに紐ずくhtmlのパスを取得
-  const route = routes[path] || routes[404];
-  // htmlを取得
-  const html = await fetch(route).then((data) => data.text());
-  // 取得したhtmlを動的にルート直下のindex.htmlに差し込む
-  document.getElementById("main-page").innerHTML = html;
-};
-
-if (typeof window !== "undefined") {
-  // windowを使う処理を記述
-  window.addEventListener("popstate", (event) => {
-    handleLocation();
-  });
-}
-
-handleLocation();
 
 app.get("/edit/:id", (req, res) => {
   const sql = "SELECT * FROM appointments WHERE id = ?";
